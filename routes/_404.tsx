996939c19896d429f6882ba5d796e3fe534ecaf5
PapaZouk <rafexpapala@gmail.com>
2024-12-22 22:02:16 +0100

    feat: update error page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,20 +6,16 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for doesn't exist.
-          </p>
-          <a href="/" class="underline">Go back home</a>
+      <div className="bg-[#020015FF] min-h-screen flex flex-col items-center justify-center">
+        <div className="px-4 py-8 mx-auto mt-[-20%]">
+          <div className="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+            <h1 className="text-white mb-4">Nie znaleziono strony</h1>
+            <p className="text-white mb-4">Nie martw się, nawet najlepszym zdarza się zgubić drogę.</p>
+            <p>
+              <img src="/images/not-found.png" style={{ width: "200px" }} alt="Not found" />
+            </p>
+            <a href="/" className="underline text-white">Wróć na stronę główną</a>
+          </div>
         </div>
       </div>
     </>
